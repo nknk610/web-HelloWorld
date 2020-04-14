89daff55196b4aa246bf88339b1c360fa9779f8e
Posiya <48229973+nknk610@users.noreply.github.com>
2020-04-14 11:20:34 +0900

    Upload Project

---
+++ helloworld/src/main/java/com/webprogramming/helloworld/HelloController.java
@@ -0,0 +1,14 @@
+package com.webprogramming.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController 
+{
+    @GetMapping
+    public String Hello()
+    {
+        return "Hello World";
+    }
+}
