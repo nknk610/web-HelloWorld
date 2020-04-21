10fbb9baa2b27583262176a843cec245e6c57886
Posiya <48229973+nknk610@users.noreply.github.com>
2020-04-21 12:13:49 +0900

    Add calculate function

--- helloworld/src/main/java/com/webprogramming/helloworld/HelloController.java
+++ helloworld/src/main/java/com/webprogramming/helloworld/HelloController.java
@@ -12,111 +12,76 @@ public class HelloController
     @GetMapping("/add")
     public String Add(@RequestParam final String param1, @RequestParam final String param2)
     {
-        float num1 = 0f, num2 = 0f;
-        try
-        {
-            num1 = Float.parseFloat(param1);
-            num2 = Float.parseFloat(param2);
-        } catch(NumberFormatException e){ return "숫자가 입력되지 않았습니다.";}
-
-        return num1 + " + " + num2 + " = " + (num1 + num2);
+        return CalculateAdd(param1, param2);
     }
 
     @GetMapping("/sub")
     public String Subtract(@RequestParam final String param1, @RequestParam final String param2)
     {
-        float num1 = 0f, num2 = 0f;
-        try
-        {
-            num1 = Float.parseFloat(param1);
-            num2 = Float.parseFloat(param2);
-        } catch(NumberFormatException e){ return "숫자가 입력되지 않았습니다.";}
-
-        return num1 + " - " + num2 + " = " + (num1 - num2);
+        return CalculateSubtract(param1, param2);
     }
 
     @GetMapping("/mul")
     public String Multiply(@RequestParam final String param1, @RequestParam final String param2)
     {
-        float num1 = 0f, num2 = 0f;
-        try
-        {
-            num1 = Float.parseFloat(param1);
-            num2 = Float.parseFloat(param2);
-        } catch(NumberFormatException e){ return "숫자가 입력되지 않았습니다.";}
-
-        return num1 + " * " + num2 + " = " + (num1 * num2);
+        return CalculateMultiply(param1, param2);
     }
 
     @GetMapping("/div")
     public String Divide(@RequestParam final String param1, @RequestParam final String param2)
     {
-        float num1 = 0f, num2 = 0f;
-        try
-        {
-            num1 = Float.parseFloat(param1);
-            num2 = Float.parseFloat(param2);
-        } catch(NumberFormatException e) {return "숫자가 입력되지 않았습니다.";}
-
-        if (num2 == 0f) return "0으로 나눌 수 없습니다";
-        return num1 + " / " + num2 + " = " + (num1 / num2);
+        return CalculateDivide(param1, param2);
     }
 
     @GetMapping("/add1/{param1}/{param2}")
     public String Add1(@PathVariable final String param1, @PathVariable final String param2)
     {
-        float num1 = 0f, num2 = 0f;
-        try
-        {
-            num1 = Float.parseFloat(param1);
-            num2 = Float.parseFloat(param2);
-        } catch(NumberFormatException e){ return "숫자가 입력되지 않았습니다.";}
-
-        return num1 + " + " + num2 + " = " + (num1 + num2);
+        return CalculateAdd(param1, param2);
     }
 
     @GetMapping("/sub1/{param1}/{param2}")
     public String Subtract1(@PathVariable final String param1, @PathVariable final String param2)
     {
-        float num1 = 0f, num2 = 0f;
-        try
-        {
-            num1 = Float.parseFloat(param1);
-            num2 = Float.parseFloat(param2);
-        } catch(NumberFormatException e){ return "숫자가 입력되지 않았습니다.";}
-
-        return num1 + " - " + num2 + " = " + (num1 - num2);
+        return CalculateSubtract(param1, param2);
     }
 
     @GetMapping("/mul1/{param1}/{param2}")
     public String Multiply1(@PathVariable final String param1, @PathVariable final String param2)
     {
-        float num1 = 0f, num2 = 0f;
-        try
-        {
-            num1 = Float.parseFloat(param1);
-            num2 = Float.parseFloat(param2);
-        } catch(NumberFormatException e){ return "숫자가 입력되지 않았습니다.";}
-
-        return num1 + " * " + num2 + " = " + (num1 * num2);
+        return CalculateMultiply(param1, param2);
     }
 
     @GetMapping("/div1/{param1}/{param2}")
     public String Divide1(@PathVariable final String param1, @PathVariable final String param2)
     {
-        float num1 = 0f, num2 = 0f;
-        try
-        {
-            num1 = Float.parseFloat(param1);
-            num2 = Float.parseFloat(param2);
-        } catch(NumberFormatException e) {return "숫자가 입력되지 않았습니다.";}
-
-        if (num2 == 0f) return "0으로 나눌 수 없습니다";
-        return num1 + " / " + num2 + " = " + (num1 / num2);
+        return CalculateDivide(param1, param2);
     }
 
     @PostMapping("/add2")
     public String Add2(@RequestParam final String param1, @RequestParam final String param2)
+    {
+        return CalculateAdd(param1, param2);
+    }
+
+    @GetMapping("/sub2")
+    public String Subtract2(@RequestParam final String param1, @RequestParam final String param2)
+    {
+        return CalculateSubtract(param1, param2);
+    }
+
+    @GetMapping("/mul2")
+    public String Multiply2(@RequestParam final String param1, @RequestParam final String param2)
+    {
+        return CalculateMultiply(param1, param2);
+    }
+
+    @GetMapping("/div2")
+    public String Divide2(@RequestParam final String param1, @RequestParam final String param2)
+    {
+        return CalculateDivide(param1, param2);
+    }
+
+    public String CalculateAdd(String param1, String param2)
     {
         float num1 = 0f, num2 = 0f;
         try
@@ -128,8 +93,7 @@ public class HelloController
         return num1 + " + " + num2 + " = " + (num1 + num2);
     }
 
-    @GetMapping("/sub2")
-    public String Subtract2(@RequestParam final String param1, @RequestParam final String param2)
+    public String CalculateSubtract(String param1, String param2)
     {
         float num1 = 0f, num2 = 0f;
         try
@@ -141,8 +105,7 @@ public class HelloController
         return num1 + " - " + num2 + " = " + (num1 - num2);
     }
 
-    @GetMapping("/mul2")
-    public String Multiply2(@RequestParam final String param1, @RequestParam final String param2)
+    public String CalculateMultiply(String param1, String param2)
     {
         float num1 = 0f, num2 = 0f;
         try
@@ -154,17 +117,16 @@ public class HelloController
         return num1 + " * " + num2 + " = " + (num1 * num2);
     }
 
-    @GetMapping("/div2")
-    public String Divide2(@RequestParam final String param1, @RequestParam final String param2)
+    public String CalculateDivide(String param1, String param2)
     {
         float num1 = 0f, num2 = 0f;
         try
         {
             num1 = Float.parseFloat(param1);
             num2 = Float.parseFloat(param2);
-        } catch(NumberFormatException e) { return "숫자가 입력되지 않았습니다.";}
+        } catch(NumberFormatException e){ return "숫자가 입력되지 않았습니다.";}
 
-        if (num2 == 0f) return "0으로 나눌 수 없습니다";
+        if(num2 == 0f) return "0으로 나눌 수 없습니다.";
         return num1 + " / " + num2 + " = " + (num1 / num2);
     }
 }
