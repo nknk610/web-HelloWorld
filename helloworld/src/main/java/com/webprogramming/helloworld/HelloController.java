23e18ee873b2cd5186617d29a155592c95015dec
Posiya <48229973+nknk610@users.noreply.github.com>
2020-04-21 12:06:50 +0900

    Add post method

--- helloworld/src/main/java/com/webprogramming/helloworld/HelloController.java
+++ helloworld/src/main/java/com/webprogramming/helloworld/HelloController.java
@@ -3,6 +3,7 @@ package com.webprogramming.helloworld;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -109,6 +110,59 @@ public class HelloController
             num1 = Float.parseFloat(param1);
             num2 = Float.parseFloat(param2);
         } catch(NumberFormatException e) {return "숫자가 입력되지 않았습니다.";}
+
+        if (num2 == 0f) return "0으로 나눌 수 없습니다";
+        return num1 + " / " + num2 + " = " + (num1 / num2);
+    }
+
+    @PostMapping("/add2")
+    public String Add2(@RequestParam final String param1, @RequestParam final String param2)
+    {
+        float num1 = 0f, num2 = 0f;
+        try
+        {
+            num1 = Float.parseFloat(param1);
+            num2 = Float.parseFloat(param2);
+        } catch(NumberFormatException e){ return "숫자가 입력되지 않았습니다.";}
+
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+
+    @GetMapping("/sub2")
+    public String Subtract2(@RequestParam final String param1, @RequestParam final String param2)
+    {
+        float num1 = 0f, num2 = 0f;
+        try
+        {
+            num1 = Float.parseFloat(param1);
+            num2 = Float.parseFloat(param2);
+        } catch(NumberFormatException e){ return "숫자가 입력되지 않았습니다.";}
+
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+
+    @GetMapping("/mul2")
+    public String Multiply2(@RequestParam final String param1, @RequestParam final String param2)
+    {
+        float num1 = 0f, num2 = 0f;
+        try
+        {
+            num1 = Float.parseFloat(param1);
+            num2 = Float.parseFloat(param2);
+        } catch(NumberFormatException e){ return "숫자가 입력되지 않았습니다.";}
+
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+
+    @GetMapping("/div2")
+    public String Divide2(@RequestParam final String param1, @RequestParam final String param2)
+    {
+        float num1 = 0f, num2 = 0f;
+        try
+        {
+            num1 = Float.parseFloat(param1);
+            num2 = Float.parseFloat(param2);
+        } catch(NumberFormatException e) { return "숫자가 입력되지 않았습니다.";}
 
         if (num2 == 0f) return "0으로 나눌 수 없습니다";
         return num1 + " / " + num2 + " = " + (num1 / num2);
